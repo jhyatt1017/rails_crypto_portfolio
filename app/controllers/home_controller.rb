f7173c5066c36910b60d06f201ea149a079662e4
lord darkseid <github email address>
2018-06-14 21:21:00 -0700

    added bootstrap and CoinMarketCap API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,49 @@
 class HomeController < ApplicationController
   def index
+    # adding moduels
+    require 'net/http'
+    require 'json'
+    # instance variable thats adding url
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    # @url = 'https://api.coinmarketcap.com/v2/ticker/'
+    # create uri (uniform resource identifer)
+    @uri = URI(@url)
+    # go to the website url
+    @response = Net::HTTP.get(@uri)
+    # grab result and parse it to the json and give the results to coins
+    @coins = JSON.parse(@response)
+    # list of currency we want to pull out
+    @my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM"]
   end
+
+  def about
+  end
+
+  def lookup
+    # adding moduels
+    require 'net/http'
+    require 'json'
+    # instance variable thats adding url
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    # @url = 'https://api.coinmarketcap.com/v2/ticker/'
+    # create uri (uniform resource identifer)
+    @uri = URI(@url)
+    # go to the website url
+    @response = Net::HTTP.get(@uri)
+    # grab result and parse it to the json and give the results to coins
+    @lookup_coin = JSON.parse(@response)
+
+    @symbol = params[:sym]
+
+    if @symbol
+      # turn symbol into upper case
+      @symbol = @symbol.upcase
+    end
+
+    # Error message if no currency is entered
+    if @symbol == ""
+      @symbol = "Hey, you forgot to enter a currency!"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
+  # homepage path
+  root 'home#index' 
+  # about page path
+  get 'home/about'
+  # lookup "search page" path
+  get 'home/lookup' 
+  post "/home/lookup" => 'home/lookup'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
